b919b991de097e95fbbe8fd192e5e934e4af2bc2
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-07 13:35:53 +0100

    Add trip interface

---
+++ Trip_Planner/Interfaces/ITrip.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trip_Planner.Models;
+
+namespace Trip_Planner.Interfaces
+{
+    public interface ITrip
+    {
+        Task<ActionResult<Trip>> CreateTrip(Trip trip);
+        Task<ActionResult<Trip>> GetTrip(int id);
+        Task<ActionResult<Trip>> UpdateTrip(int id, UpdateTripModel updateTripModel);
+        Task<ActionResult> DeleteTrip(int id);
+    }
+}
