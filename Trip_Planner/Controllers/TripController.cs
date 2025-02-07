1b0be0c0625056ab3f5d5bc00abff173362f5233
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-07 13:33:22 +0100

    Remove unused controller, add Trip Controller

---
+++ Trip_Planner/Controllers/TripController.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Trip_Planner.Interfaces;
+using Trip_Planner.Models;
+using Trip_Planner.Services;
+
+namespace Trip_Planner.Controllers
+{
+    [ApiController]
+    [Route("api/trips")]
+    public class TripController : ControllerBase
+    {
+        private readonly ITrip _tripService;
+        public TripController(ITrip tripService)
+        {
+            _tripService = tripService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Trip>> CreateTrip([FromBody] CreateTripModel createTripModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data provided.");
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized("User ID not found in token.");
+            }
+            int parsedUserId = int.Parse(userId);
+
+            var trip = new Trip
+            {
+                TripName = createTripModel.TripName,
+                Destination = createTripModel.Destination,
+                Description = createTripModel.Description,
+                StartDate = createTripModel.StartDate,
+                EndDate = createTripModel.EndDate,
+                UserId = parsedUserId
+            };
+
+            return await _tripService.CreateTrip(trip);
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trip>> GetTrip(int id)
+        {
+            return await _tripService.GetTrip(id);
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Trip>> UpdateTrip(int id, [FromBody] UpdateTripModel updateTripModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data provided.");
+            }
+            return await _tripService.UpdateTrip(id, updateTripModel);
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteTrip(int id)
+        {
+            return await _tripService.DeleteTrip(id);
+        }
+
+    }
+}
