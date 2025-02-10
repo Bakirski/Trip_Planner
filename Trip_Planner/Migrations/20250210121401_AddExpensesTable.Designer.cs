cf4426e1729eb567f99098c55a18704428792c8f
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-10 14:15:50 +0100

    Reapply "Add Expenses Table"
    This reverts commit 2e739cfed6472bd837d7cfee1412d5843a31c272.

--- Trip_Planner/Migrations/20250210121401_AddExpensesTable.Designer.cs
+++ Trip_Planner/Migrations/20250210121401_AddExpensesTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Trip_Planner.Data;
@@ -11,9 +12,11 @@ using Trip_Planner.Data;
 namespace Trip_Planner.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250210121401_AddExpensesTable")]
+    partial class AddExpensesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
