4bb4daf2906316f52df49b1fc97d8bc6a6799fb6
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-19 13:07:12 +0100

    Add separate js file for registration

---
+++ frontend/Scripts/register.js
@@ -0,0 +1,51 @@
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("Form submission intercepted.");
+
+    const username = document.getElementById("userName").value;
+    const email = document.getElementById("userEmail").value;
+    const password = document.getElementById("userPassword").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    try {
+      await registerUser(username, email, password, confirmPassword);
+      window.location.href = "/";
+    } catch (error) {
+      console.error("Error registering user: ", error);
+    }
+  });
+
+async function registerUser(
+  username,
+  userEmail,
+  userPassword,
+  confirmPassword
+) {
+  try {
+    const response = await fetch("http://localhost:5063/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Username: username,
+        Email: userEmail,
+        Password: userPassword,
+        ConfirmPassword: confirmPassword,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(errorMessage);
+    }
+
+    const data = await response.json();
+    console.log("Register Success: ", data);
+    alert("You have registered successfully.");
+  } catch (error) {
+    throw error;
+  }
+}
--- frontend/Scripts/script.js
+++ frontend/Scripts/script.js
@@ -63,57 +63,8 @@ async function authenticateUser(userEmail, userPassword) {
   }
 }
 
-//USER REGISTRATION
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementById("registerForm")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
-
-      const username = document.getElementById("userName").value;
-      const email = document.getElementById("userEmail").value;
-      const password = document.getElementById("userPassword").value;
-      const confirmPassword = document.getElementById("confirmPassword").value;
-
-      try {
-        await registerUser(username, email, password, confirmPassword);
-        window.location.href = "/index.html";
-      } catch (error) {
-        console.error("Error registering user: ", error);
-      }
-    });
-
-  async function registerUser(
-    username,
-    userEmail,
-    userPassword,
-    confirmPassword
-  ) {
-    try {
-      const response = await fetch("http://localhost:5063/api/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          Username: username,
-          Email: userEmail,
-          Password: userPassword,
-          ConfirmPassword: confirmPassword,
-        }),
-      });
-
-      if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-      }
-
-      const data = await response.json();
-      console.log("Register Success: ", data);
-    } catch (error) {
-      throw error;
-    }
-  }
+window.addEventListener("load", function () {
+  console.log("Window fully loaded!");
 });
 
 //USER DETAIL FETCH
