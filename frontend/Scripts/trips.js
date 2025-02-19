61f22c82addeea101b268fa626fb4340136f9483
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-19 09:14:53 +0100

    Fix live injection and add functionality to create trips

--- frontend/Scripts/trips.js
+++ frontend/Scripts/trips.js
@@ -25,18 +25,77 @@ async function getTrips() {
 function displayData(data) {
   const dataContainer = document.getElementById("dataContainer");
 
-  dataContainer.innerHTML = "";
-
   data.forEach((item) => {
-    const dataItem = document.createElement("div");
+    const dataItem = document.createElement("tr");
     dataItem.classList.add("data-item");
-    dataItem.textContent = `
-      ID: ${item.id},
-      Name: ${item.tripName}, 
-      Destination: ${item.destination}, 
-      Description: ${item.description}, 
-      Start Date: ${item.startDate}, 
-      End Date: ${item.endDate}`;
+
+    dataItem.innerHTML = `
+      <td>${item.tripName}</td> 
+      <td>${item.destination}</td> 
+      <td>${
+        item.description && item.description.trim()
+          ? item.description
+          : "No Description"
+      }</td>
+      <td>${item.startDate}</td>
+      <td>${item.endDate}</td>
+      <td><button value=${item.Id}>Update Trip</button></td>
+      <td><button value=${item.Id}>Delete Trip</button></td>`;
     dataContainer.appendChild(dataItem);
   });
+}
+
+function toggleForm() {
+  const displayForm = document.getElementById("createTripForm");
+  displayForm.classList.toggle("hidden");
+}
+
+document
+  .getElementById("createTripForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const tripName = document.getElementById("tripName").value;
+    const tripDestination = document.getElementById("destination").value;
+    const tripDescription = document.getElementById("description").value;
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+
+    await createTrip(
+      tripName,
+      tripDestination,
+      tripDescription,
+      startDate,
+      endDate
+    );
+    location.reload();
+  });
+
+async function createTrip(
+  tripName,
+  tripDestination,
+  tripDescription,
+  startDate,
+  endDate
+) {
+  try {
+    const response = await fetch("http://localhost:5063/api/trips", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${bearerToken}`,
+      },
+      body: JSON.stringify({
+        TripName: tripName,
+        Destination: tripDestination,
+        Description: tripDescription,
+        StartDate: startDate,
+        EndDate: endDate,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error creating new trip: ", error);
+  }
 }
