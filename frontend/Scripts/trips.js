b03e2a7504b98a234af39ea490d2362fcdd592f3
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-19 10:34:50 +0100

    Add functionality for updating trips

--- frontend/Scripts/trips.js
+++ frontend/Scripts/trips.js
@@ -39,13 +39,14 @@ function displayData(data) {
       }</td>
       <td>${item.startDate}</td>
       <td>${item.endDate}</td>
-      <td><button value=${item.id}>Update Trip</button></td>
+      <td><button
+       onclick="toggleUpdateForm(${item.id})">Update Trip</button></td>
       <td><button onclick="deleteTrip(${item.id})">Delete Trip</button></td>`;
     dataContainer.appendChild(dataItem);
   });
 }
 
-function toggleForm() {
+function toggleCreateForm() {
   const displayForm = document.getElementById("createTripForm");
   displayForm.classList.toggle("hidden");
 }
@@ -97,6 +98,66 @@ async function createTrip(
     console.log(data);
   } catch (error) {
     console.error("Error creating new trip: ", error);
+  }
+}
+
+function toggleUpdateForm(id) {
+  sessionStorage.setItem("id", id);
+  const displayForm = document.getElementById("updateTripForm");
+  displayForm.classList.toggle("hidden");
+}
+
+document
+  .getElementById("updateTripForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const id = sessionStorage.getItem("id");
+    const tripName = document.getElementById("updateTripName").value;
+    const tripDestination = document.getElementById("updateDestination").value;
+    const tripDescription = document.getElementById("updateDescription").value;
+    const startDate = document.getElementById("updateStartDate").value;
+    const endDate = document.getElementById("updateEndDate").value;
+    console.log(startDate);
+
+    await updateTrip(
+      id,
+      tripName,
+      tripDestination,
+      tripDescription,
+      startDate,
+      endDate
+    );
+    location.reload();
+  });
+
+async function updateTrip(
+  id,
+  tripName,
+  tripDestination,
+  tripDescription,
+  startDate,
+  endDate
+) {
+  try {
+    const response = await fetch(`http://localhost:5063/api/trips/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${bearerToken}`,
+      },
+      body: JSON.stringify({
+        TripName: tripName,
+        Destination: tripDestination,
+        Description: tripDescription,
+        StartDate: startDate,
+        EndDate: endDate,
+      }),
+    });
+    alert("Trip updated successfully.");
+    sessionStorage.removeItem("id");
+  } catch (error) {
+    console.error("Error updating trip: ", error);
   }
 }
 
