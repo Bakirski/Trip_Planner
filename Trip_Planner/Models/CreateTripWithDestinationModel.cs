186358a8e66eb8c39b8f7b348cb76573307b91ff
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-20 13:30:40 +0100

    Create composite model for trips and destinations

---
+++ Trip_Planner/Models/CreateTripWithDestinationModel.cs
@@ -0,0 +1,11 @@
+﻿using Trip_Planner.Models.Destinations;
+using Trip_Planner.Models.Trips;
+
+namespace Trip_Planner.Models
+{
+    public class CreateTripWithDestinationModel
+    {
+        public CreateTripModel Trip { get; set; }
+        public CreateDestinationModel Destination { get; set; }
+    }
+}
