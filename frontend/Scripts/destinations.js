4adc2ec089471fd78e229f35de148e32e1e17b40
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-02-22 19:46:01 +0100

    Add functionality for destination deletion

--- frontend/Scripts/destinations.js
+++ frontend/Scripts/destinations.js
@@ -24,6 +24,7 @@ async function getTrip(id) {
     );
 
     const tripData = await response.json();
+    sessionStorage.setItem("tripId", tripData.id);
     const destinationData = await response2.json();
 
     console.log(tripData);
@@ -46,8 +47,70 @@ function displayData(data) {
   if (data[1].length > 0) {
     data[1].forEach((item) => {
       const dataItem = document.createElement("p");
-      dataItem.textContent = item.destinationName;
+      dataItem.innerHTML = `
+      ${item.destinationName}
+      <button onclick="deleteDestination(${item.tripId}, ${item.id})"">Delete</button>`;
       destinationsContainer.appendChild(dataItem);
     });
   } else null;
+}
+
+async function deleteDestination(tripId, destinationId) {
+  try {
+    const response = await fetch(
+      `http://localhost:5063/api/trips/${tripId}/destinations/${destinationId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${bearerToken}`,
+        },
+      }
+    );
+
+    alert("Destination successfully deleted.");
+    location.reload();
+  } catch (error) {
+    console.error("Error deleting destination: ", error);
+  }
+}
+
+function toggleDestinationForm() {
+  const displayForm = document.getElementById("destinationForm");
+  displayForm.classList.toggle("hidden");
+}
+
+document
+  .getElementById("destinationForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const destinationName = document.getElementById("destinationName").value;
+    createDestination(destinationName);
+  });
+
+async function createDestination(destinationName) {
+  const id = sessionStorage.getItem("tripId");
+  try {
+    const response = await fetch(
+      `http://localhost:5063/api/trips/${id}/destinations`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${bearerToken}`,
+        },
+        body: JSON.stringify({
+          DestinationName: destinationName,
+        }),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    alert("Destination created successfully.");
+    location.reload();
+  } catch (error) {
+    console.error("Error creating destination: ", error);
+  }
 }
